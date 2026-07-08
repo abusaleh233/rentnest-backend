acd367bed19c4d77cf8c6fc0907ed1df4d79dcb6
abusaleh233 <abusaleh233@gmail.com>
2026-07-08 12:38:54 +0600

    Implement Admin Dashboard APIs

--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -5,7 +5,7 @@ import { prisma } from '../../lib/prisma';
 import ApiError from '../../errors/ApiError';
 
 const stripe = new Stripe(config.stripe.secret_key as string, {
-  apiVersion: '2025-01-27' as any,
+  apiVersion: '2025-01-27' as any, 
 });
 
 const createPaymentIntent = async (userId: string, rentalRequestId: string) => {
@@ -17,11 +17,11 @@ const createPaymentIntent = async (userId: string, rentalRequestId: string) => {
     throw new ApiError(httpStatus.NOT_FOUND, 'Rental request not found');
   }
 
+
   if (rentalRequest.status !== 'APPROVED') {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Rental request is not approved yet');
   }
 
-  
   const amountInCents = Math.round(rentalRequest.totalPrice * 100);
 
   const paymentIntent = await stripe.paymentIntents.create({
@@ -39,19 +39,29 @@ const createPaymentIntent = async (userId: string, rentalRequestId: string) => {
 };
 
 const confirmPayment = async (userId: string, payload: { transactionId: string; rentalRequestId: string }) => {
-  return await prisma.$transaction(async (tx:any) => {
+  return await prisma.$transaction(async (tx: any) => {
+    
+    
+    const rentalRequest = await tx.rentalRequest.findUnique({
+      where: { id: payload.rentalRequestId }
+    });
+
+    if (!rentalRequest) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Rental request not found');
+    }
+
     
     const payment = await tx.payment.create({
       data: {
         userId,
         rentalRequestId: payload.rentalRequestId,
-        amount: 0, 
+        amount: rentalRequest.totalPrice, 
         transactionId: payload.transactionId,
         status: 'COMPLETED',
       },
     });
 
-    
+  
     await tx.rentalRequest.update({
       where: { id: payload.rentalRequestId },
       data: { status: 'APPROVED' }, 
